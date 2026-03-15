00478cae8321ba4f7ef7e3412c4abb95a0da3243
rhnmrgrtt <rhnmrgrtt.herrera@gmail.com>
2026-03-15 20:25:01 +0800

    updated done exercise!

--- M5E-DjangoTemplates-HerreraLatonioSy/tapas/urls.py
+++ M5E-DjangoTemplates-HerreraLatonioSy/tapas/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from tapasapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tapasapp.urls')),
+    path('', views.view_menu, name='view_menu'),
+    path('add_menu', views.add_menu, name='add_menu'),
 ]
